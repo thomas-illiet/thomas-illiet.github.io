386835a9eb5a3ac5e09b7ae305374c86e9b80a51
Thomas ILLIET <contact@thomas-illiet.fr>
2025-09-21 19:14:13 +0200

    Improve pdf generator

--- assets/js/pdf-generator.js
+++ assets/js/pdf-generator.js
@@ -26,7 +26,7 @@ function generatePDF() {
 
       // Configure pdf options
       const opt = {
-        margin: 10,
+        margin: [0, -0.1, 0, 0],
         filename: filename,
         image: { type: "jpeg", quality: 0.98 },
         html2canvas: {
